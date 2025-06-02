086c9de04e6f2d478226e19000f3151fa25517c3
WitcherTheWhite <1240749052@qq.com>
2025-06-02 15:27:43 +0800

    支持 Redis 的 List 数据结构

--- redis/types.go
+++ redis/types.go
@@ -311,3 +311,103 @@ func (rds *RedisDataStructure) SRem(key, member []byte) (bool, error) {
 
 	return true, nil
 }
+
+// ======================= List 数据结构 =======================
+
+func (rds *RedisDataStructure) LPush(key, element []byte) (uint32, error) {
+	return rds.pushInner(key, element, true)
+}
+
+func (rds *RedisDataStructure) RPush(key, element []byte) (uint32, error) {
+	return rds.pushInner(key, element, false)
+}
+
+func (rds *RedisDataStructure) pushInner(key, element []byte, isLeft bool) (uint32, error) {
+	meta, err := rds.findMetadata(key, List)
+	if err != nil {
+		return 0, err
+	}
+
+	lk := &listInternalKey{
+		key:     key,
+		version: meta.version,
+	}
+	if isLeft {
+		lk.index = meta.head - 1
+	} else {
+		lk.index = meta.tail
+	}
+	encKey := lk.encode()
+
+	wb := rds.db.NewWriteBatch(bitcask.DefaultWriteBatchOptions)
+	if err := wb.Put(encKey, element); err != nil {
+		return 0, err
+	}
+	meta.size++
+	if isLeft {
+		meta.head--
+	} else {
+		meta.tail++
+	}
+	if err := wb.Put(key, meta.encode()); err != nil {
+		return 0, err
+	}
+	if err := wb.Commit(); err != nil {
+		return 0, err
+	}
+
+	return meta.size, nil
+}
+
+func (rds *RedisDataStructure) LPop(key []byte) ([]byte, error) {
+	return rds.popInner(key, true)
+}
+
+func (rds *RedisDataStructure) RPop(key []byte) ([]byte, error) {
+	return rds.popInner(key, false)
+}
+
+func (rds *RedisDataStructure) popInner(key []byte, isLeft bool) ([]byte, error) {
+	meta, err := rds.findMetadata(key, List)
+	if err != nil {
+		return nil, err
+	}
+	if meta.size == 0 {
+		return nil, nil
+	}
+
+	lk := &listInternalKey{
+		key:     key,
+		version: meta.version,
+	}
+	if isLeft {
+		lk.index = meta.head
+	} else {
+		lk.index = meta.tail - 1
+	}
+	encKey := lk.encode()
+
+	element, err := rds.db.Get(encKey)
+	if err != nil {
+		return nil, err
+	}
+
+	wb := rds.db.NewWriteBatch(bitcask.DefaultWriteBatchOptions)
+	if err := wb.Delete(encKey); err != nil {
+		return nil, err
+	}
+	meta.size--
+	if isLeft {
+		meta.head++
+	} else {
+		meta.tail--
+	}
+	if err := wb.Put(key, meta.encode()); err != nil {
+		return nil, err
+	}
+	if err := wb.Commit(); err != nil {
+		return nil, err
+	}
+
+	return element, nil
+}
